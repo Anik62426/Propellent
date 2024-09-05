2a20a750797308296019e169287f8a9957fec6fa
pappukumar16 <pappusk1293@gmail.com>
2024-09-05 23:55:07 +0530

    Pr-06 solve

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import Infinity from "../component/Infogood"
-import Upcomming from "../component/Upcoming"
-import SignIn from "../component/SignIn"
+
 
 const Home = () => {
   return (
     <div>
-      <SignIn/>
-       <Infinity/>
-       <Upcomming/>
+      
     </div>
     
   )
