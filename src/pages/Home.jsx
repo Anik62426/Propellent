e983d26411f6286fcaf8ca0d0a33076d0a3b6153
Aniket Kumar <aniketkumaratl@gmail.com>
2024-09-05 23:40:38 +0530

    added some changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
-
+import SignIn from '../component/SignIn'
+import Upcoming from '../component/Upcoming'
+import Infogood from '../component/Infogood'
 const Home = () => {
   return (
     <div>
- 
+      <SignIn/>
+      <Infogood/>
+      <Upcoming/>
+    
     </div>
     
   )
