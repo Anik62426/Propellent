6b00137264df7096bfa56baf43c14d0223b38bb3
Aniket Kumar <aniketkumaratl@gmail.com>
2024-09-05 20:21:42 +0530

    solving conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage:{
+        'footer-texture': "url('./src/assets/footer-texture.jpeg')",
+      }
     },
   },
   plugins: [],
